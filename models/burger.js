110a7bbf003794edd9a118c6ec7addc18099b6ee
Daniel Tovar <daniel@danieltovar.io>
2019-05-13 15:32:30 -0600

    Created Sequelize model for Burger

--- models/burger.js
+++ models/burger.js
@@ -1,40 +1,28 @@
-// * The Requires
-// * ========================================
-const orm = require('../config/orm');
-
-// * The Model
-// * ========================================
-// ! Creating a burger model that will take the orm funcs and
-// ! apply them specifically for my burgers
-const burger = {
-    all: function(cb) {
-        // ! Pass the table 'burger'
-        orm.selectAll('burger', function(r) {
-            cb(r);
-        });
-    },
-    add: function(cols, vals, cb) {
-        // ! Pass the table 'burger'
-        orm.insertOne('burger', cols, vals, function(r) {
-            cb(r);
-        });
-    },
-    update: function(data, condition, cb) {
-        // ! Pass the table 'burger'
-        orm.updateOne('burger', data, condition, function(r) {
-            cb(r);
-        });
-    }
-};
-
+// ! Export the model
 module.exports = function(sequelize, DataTypes) {
+    // ! Define the new Burger model (a blueprint for my db table)
     let Burger = sequelize.define('Burger', {
+        // ! Define a column called 'burger_name
         burger_name: {
+            // ! It'a type is string
             type: DataTypes.STRING,
+            // ! It does not allow a null value
             allowNull: false,
+            // ! It's legnth must be between 10 and 140 characters
             validate: {
-                len: [1, 140]
+                len: [10, 140]
             }
+        },
+        // ! Define a column called Devoured
+        devoured: {
+            // ! It's type is boolean
+            type: DataTypes.BOOLEAN,
+            // ! It's default value is false
+            defaultValue: false,
+            // ! It does not allow a null value
+            allowNull: false
         }
     });
+    // ! Return Burger so it can be used
+    return Burger;
 };
